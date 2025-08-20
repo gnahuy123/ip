7f3239eb1954471c0d6d769c02f76a18e59b2b2d
gnahuy123 <e1398507@u.nus.edu>
2025-08-20 16:23:52 +0800

    Allow users to add tasks and display back to them when requested

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Lebron {
@@ -5,6 +7,7 @@ public class Lebron {
         Scanner myObj = new Scanner(System.in);
         String welcomeMsg = "Wassup, I'm Lebron. What popping homie?";
         String exitMsg = "Im gonna bounce. See ya fam!";
+        List<String> myList = new ArrayList<>();
 
         //print welcome message
         System.out.println(welcomeMsg);
@@ -12,8 +15,14 @@ public class Lebron {
 
         String userInput = myObj.nextLine();
         while (!userInput.equals("bye")) {
+
             printHorizontalLine();
-            System.out.println(userInput);
+            if (userInput.equals("list")) {
+                printList(myList);
+            } else {
+                myList.add(userInput);
+                System.out.println("added: " + userInput);
+            }
             printHorizontalLine();
             userInput = myObj.nextLine();
         }
@@ -33,5 +42,15 @@ public class Lebron {
         }
 
         System.out.println();
+    }
+
+    public static void printList(List<String> ls) {
+        if (ls.isEmpty()) {
+            System.out.println("List is Empty");
+        } else {
+            for (int i = 1; i < ls.size() + 1; i++) {
+                System.out.println(i + ". " + ls.get(i-1));
+            }
+        }
     }
 }
