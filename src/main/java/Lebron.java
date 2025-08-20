e102a23c8ff8b839654fffbf5deeffe5467aef37
gnahuy123 <e1398507@u.nus.edu>
2025-08-20 16:05:46 +0800

    Allowed Chatbot to echo commands

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,16 +1,26 @@
+import java.util.Scanner;
+
 public class Lebron {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
         String welcomeMsg = "Wassup, I'm Lebron. What popping homie?";
         String exitMsg = "Im gonna bounce. See ya fam!";
 
-        printHorizontalLine();
-
+        //print welcome message
         System.out.println(welcomeMsg);
-
         printHorizontalLine();
 
-        System.out.println(exitMsg);
+        String userInput = myObj.nextLine();
+        while (!userInput.equals("bye")) {
+            printHorizontalLine();
+            System.out.println(userInput);
+            printHorizontalLine();
+            userInput = myObj.nextLine();
+        }
+        printHorizontalLine();
 
+        //print exit message
+        System.out.println(exitMsg);
         printHorizontalLine();
     }
 
